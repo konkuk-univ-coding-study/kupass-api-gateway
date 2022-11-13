65bc3f00a2dfd220a86dc452823b2babcdab7864
kyuhyeongkwon <akcps1tp@naver.com>
2022-11-13 22:41:51 +0900

    feat: api server main

---
+++ src/main/java/konkuk/kupassapi/KupassApiApplication.java
@@ -0,0 +1,13 @@
+package konkuk.kupassapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KupassApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KupassApiApplication.class, args);
+	}
+
+}
